be517ad1ee578cedb7e414058df856a2e6c11e53
Henry Gerlach <henry.gerlachmorten@gmail.com>
2026-02-18 20:53:33 +0100

    add: exercises part2e.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,78 +1,115 @@
-import axios from 'axios'
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import Filter from './components/Filter'
-import AddEntry from './components/AddEntry'
-import Numbers from './components/Numbers'
+import "./index.css";
 
-import personsService from './services/persons' 
+import Filter from "./components/Filter";
+import AddEntry from "./components/AddEntry";
+import Numbers from "./components/Numbers";
+import personsService from "./services/persons";
 
+const Notification = ({ notification }) => {
+  if (notification === null) {
+    return null;
+  }
+
+  return <div className="notification">{notification}</div>;
+};
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newFilter, setNewFilter] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
-    personsService.getAll().then((data) => 
-      setPersons(data)
-    )
-  }, [])
+    personsService.getAll().then((data) => setPersons(data));
+  }, []);
+
+  const updateNotification = (message) => {
+    setNotification(message);
+    setTimeout(() => {
+      setNotification(null);
+    }, 5000);
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   const handleFilterChange = (event) => {
-    setNewFilter(event.target.value)
-  }
+    setNewFilter(event.target.value);
+  };
 
   const handleEntrySubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     const newPerson = {
       name: newName,
-      number: newNumber
-    }
+      number: newNumber,
+    };
 
-    if (persons.some(person => person.name === newName)) {
-      if (confirm(`${newPerson.name} is already added to phonebook, replace the old number with a new one?`)) {
-        const oldPerson = persons.find(person => person.name === newName)
-        personsService.update({...newPerson, id: oldPerson.id})
-        setPersons(persons.filter(person => person.name !== newName).concat(newPerson))
+    if (persons.some((person) => person.name === newName)) {
+      if (
+        confirm(
+          `${newPerson.name} is already added to phonebook, replace the old number with a new one?`,
+        )
+      ) {
+        const oldPerson = persons.find((person) => person.name === newName);
+        personsService.update({ ...newPerson, id: oldPerson.id });
+        setPersons(
+          persons.filter((person) => person.name !== newName).concat(newPerson),
+        );
+        updateNotification(`Updated ${newPerson.name}`);
       }
     } else {
-      personsService.create(newPerson)
-      setPersons(persons.concat(newPerson))
+      personsService.create(newPerson);
+      setPersons(persons.concat(newPerson));
+      updateNotification(`Added ${newPerson.name}`);
     }
 
-    setNewName('')
-    setNewNumber('')
-  }
+    setNewName("");
+    setNewNumber("");
+  };
 
-  const handleDelete = (( id ) => {
-    setPersons(persons.filter(person => person.id !== id))
-    personsService._delete(id)
-  })  
+  const handleDelete = (id) => {
+    const deletePerson = persons.find((person) => person.id === id);
+
+    setPersons(persons.filter((person) => person.id !== id));
+
+    let error_occurred = False;
+    personsService._delete(id).catch(() => {
+      error_occurred = True;
+      updateNotification(
+        `Information for ${deletePerson.name} has already been removed from server`,
+      );
+    });
+
+    if (!error_occurred) updateNotification(`Deleted ${deletePerson.name}`);
+  };
 
   return (
     <div>
+      <Notification notification={notification} />
       <Filter value={newFilter} handleChange={handleFilterChange} />
-      <AddEntry 
-        name={newName} 
-        number={newNumber} 
+      <AddEntry
+        name={newName}
+        number={newNumber}
         handleEntrySubmit={handleEntrySubmit}
-        handleNameChange={handleNameChange} 
-        handleNumberChange={handleNumberChange} 
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
       />
-      <Numbers persons={persons} filter={newFilter} handleDelete={handleDelete} />
+      <Numbers
+        persons={persons}
+        filter={newFilter}
+        handleDelete={handleDelete}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
