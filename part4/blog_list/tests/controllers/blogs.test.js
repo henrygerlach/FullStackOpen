6ecb3875cba1aa300c64ee671c58660dc0262941
Henry Gerlach <henry.gerlachmorten@gmail.com>
2026-03-13 19:19:22 +0100

    add exercise 4.9 & 4.10

--- part4/blog_list/tests/controllers/blogs.test.js
+++ part4/blog_list/tests/controllers/blogs.test.js
@@ -1,4 +1,5 @@
 const { test, after, beforeEach } = require("node:test");
+const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../../index");
@@ -8,52 +9,46 @@ const api = supertest(app);
 
 const blogs = [
   {
-    _id: "5a422a851b54a676234d17f7",
+    id: "5a422a851b54a676234d17f7",
     title: "React patterns",
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
     likes: 7,
-    __v: 0,
   },
   {
-    _id: "5a422aa71b54a676234d17f8",
+    id: "5a422aa71b54a676234d17f8",
     title: "Go To Statement Considered Harmful",
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
-    __v: 0,
   },
   {
-    _id: "5a422b3a1b54a676234d17f9",
+    id: "5a422b3a1b54a676234d17f9",
     title: "Canonical string reduction",
     author: "Edsger W. Dijkstra",
     url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
     likes: 12,
-    __v: 0,
   },
   {
-    _id: "5a422b891b54a676234d17fa",
+    id: "5a422b891b54a676234d17fa",
     title: "First class tests",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
     likes: 10,
-    __v: 0,
   },
   {
-    _id: "5a422ba71b54a676234d17fb",
+    id: "5a422ba71b54a676234d17fb",
     title: "TDD harms architecture",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
     likes: 0,
-    __v: 0,
   },
   {
-    _id: "5a422bc61b54a676234d17fc",
+    id: "5a422bc61b54a676234d17fc",
     title: "Type wars",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
     likes: 2,
-    __v: 0,
   },
 ];
 
@@ -66,11 +61,46 @@ beforeEach(async () => {
   }
 });
 
-test("notes are returned as json", async () => {
+test("blogs are returned as json", async () => {
   await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
+});
+
+test("unique identifier is named id", async () => {
+  const response = await api.get("/api/blogs").expect(200);
+
+  for (const blog of response.body) {
+    assert.ok("id" in blog, "blog should have an id field");
+    assert.ok(!("_id" in blog), "blog should not have an _id field");
+  }
+});
+
+test("blogs are posted correctly", async () => {
+  const newBlog = {
+    title: "My Blog",
+    author: "Harry Hirsch",
+    url: "my.blog.com",
+    likes: 67,
+  };
+
+  const countBefore = await Blog.countDocuments({});
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .set("Content-Type", "application/json")
+    .expect(201);
+
+  const countAfter = await Blog.countDocuments({});
+  assert.strictEqual(countAfter, countBefore + 1);
+
+  const saved = await Blog.findOne({ title: newBlog.title });
+  assert.strictEqual(saved.title, newBlog.title);
+  assert.strictEqual(saved.author, newBlog.author);
+  assert.strictEqual(saved.url, newBlog.url);
+  assert.strictEqual(saved.likes, newBlog.likes);
 });
 
 after(async () => {
